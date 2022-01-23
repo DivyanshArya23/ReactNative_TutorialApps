cef1d8b71a81c9a264328cb45958181844347cd1
DivyanshArya23 <divyansharya20@gmail.com>
2022-01-23 12:14:33 +0530

    Video 273

--- src/screens/TrackApp/TrackDetail.jsx
+++ src/screens/TrackApp/TrackDetail.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView, { Polyline } from 'react-native-maps';
 import { Context as TrackContext } from '../../utils/context/TrackContext';
 
 const TrackDetail = function ({ navigation }) {
@@ -10,6 +11,21 @@ const TrackDetail = function ({ navigation }) {
   return (
     <View>
       <Text style={styles.title}>{track.name}</Text>
+      <MapView
+        initialRegion={{
+          ...track.locations[0].coords,
+          //   latitude: 28.95779925200964,
+          //   longitude: 77.74646883189935,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}
+        style={styles.map}
+      >
+        <Polyline
+          lineDashPattern={[1]}
+          coordinates={track?.locations?.map((loc) => loc.coords)}
+        />
+      </MapView>
     </View>
   );
 };
@@ -19,5 +35,8 @@ export default TrackDetail;
 const styles = StyleSheet.create({
   title: {
     fontSize: 48,
+  },
+  map: {
+    height: 250,
   },
 });
